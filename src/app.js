6530e75a2dfb427e3b5f4df017d23a82fd53e78d
mayconBertucci <gekesonmaycon@hotmail.com>
2020-11-24 13:24:53 +0100

    Versión final

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`Server listening to port ${port}`);
