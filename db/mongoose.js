9142a435bb4bb87889fcd708e3ec0ebc490c1b0a
mayconBertucci <gekesonmaycon@hotmail.com>
2020-11-24 13:07:10 +0100

    Versión final

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,14 @@ const { Mongoose } = require("mongoose");
 
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.bijll.mongodb.net/blogDB?retryWrites=true&w=majority", {
+/* mongoose.connect("mongodb+srv://admin:admin@cluster0.bijll.mongodb.net/blogDB?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}); */
+
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
